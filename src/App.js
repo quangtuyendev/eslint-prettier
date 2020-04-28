1b238921010991230f4ff12faed018faa226825b
Nguyễn Quang Tuyền <quangtuyennguyen09@gmail.com>
2020-04-28 20:53:16 +0700

    [master] update code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 
 function App() {
-  const header = <header className="App-header">This is app!</header>;
+  const header = <header className="App-header">This is app</header>;
   return <div className="App">{header}</div>;
-};
-  export default App;
+}
+export default App;
